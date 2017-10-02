8f4a0b416b71fc5049d0a3ba20d53389d690de9f
jeeva91 <jee1199@gmail.com>
2017-10-01 22:50:18 -0400

    try cwd

---
+++ get_cwd.c
@@ -0,0 +1,27 @@
+/*my getcwd test
+ *author:jeeva
+ * date: 09/01/2017
+ */
+
+#include<stdio.h>
+#include<string.h>
+#include<errno.h>
+#include<unistd.h>
+#include<stdlib.h>
+//#include<sys/types.h>
+//#include<sys/stat.h>
+
+
+#define BUFFSIZE 50
+
+int
+main(void){
+  char *dirname;
+  if((dirname = getcwd(dirname, BUFFSIZE))==NULL){
+      fprintf(stderr, "can't get the CWD %s", strerror(errno));
+      exit(EXIT_FAILURE);
+    }
+  printf("the directory is %s", dirname);
+  return 0;
+  
+}
