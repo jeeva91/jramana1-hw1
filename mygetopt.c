acf139e0f1f838b296b4895d2b0b235ce4ab918c
jeeva91 <jee1199@gmail.com>
2017-10-04 22:46:55 -0400

    added break, and if for opts

--- mygetopt.c
+++ mygetopt.c
@@ -12,11 +12,8 @@
 #define FILENAMESIZE 255
 
 int main(int argc, char *argv[]){
-  //  Å|AacCdFfhiklnqRrSstuwx1
-
   int A, a, c, C, d, F, f, h, i, k, l, n, q, R, r, S, s, t, u, w, x, one = 0;
   char *fileptr, *current_fileptr; //ptrs to get the operands(filenames)
-  //  char filename[FILENAMESIZE];
   int filenms_memsize =5;          // number of filenames the fileptr can hold
   int filecnt = 0;                 // number of filenames present in the fileptr
   int opt;
@@ -33,49 +30,75 @@ int main(int argc, char *argv[]){
       switch(opt){
       case 'A':
 	A = 1;
+	break;
       case 'a':
 	a = 1;
+	break;
       case 'c':
 	c = 1;
+	break;
       case 'C':
 	C = 1;
+	break;
       case 'd':
 	d = 1;
+	break;
       case 'F':
 	F = 1;
+	break;
       case 'f':
 	f = 1;
+	break;
       case 'h':
 	h = 1;
+	break;
       case 'i':
 	i = 1;
+	break;
       case 'k':
 	k = 1;
+	break;
       case 'l':
 	l = 1;
+	break;
       case 'n':
 	n = 1;
+	break;
       case 'q':
 	q = 1;
+	break;
       case 'R':
 	R = 1;
+	break;
       case 'r':
 	r = 1;
+	break;
       case 'S':
 	S = 1;
+	break;
       case 's':
 	s = 1;
+	break;
       case 't':
 	t = 1;
+	break;
       case 'u':
 	u =1;
+	break;
       case 'w':
 	w = 1;
+	break;
       case 'x':
 	x = 1;
+	break;
       case '1':
 	one =1;
-
+	break;
+      default:
+	printf("this is ls");
+	/*
+	 *print the usage
+	 */
       }	
     }
     /*
@@ -97,7 +120,53 @@ int main(int argc, char *argv[]){
     }
   }
 
-  if(A)
+  if(A){
+  }
+  if(a){
+  }
+  if(c){
+  }
+  if(C){
+  }
+  if(d){
+  }
+  if(F){
+  }
+  if(f){
+  }
+  if(h){
+  }
+  if(i){
+  }
+  if(k){
+  }
+  if(l){
+  }
+  if(n){
+  }
+  if(q){
+  }
+  if(R){
+  }
+  if(r){
+  }
+  if(S){
+  }
+  if(s){
+  }
+  if(t){
+  }
+  if(u){
+  }
+  if(w){
+  }
+  if(x){
+  }
+  if(one){
+  }
+  
+  
+  
     printf("got A\n");
   current_fileptr = fileptr;
   for(iterator = 0;iterator<filecnt; iterator++){
